c1a2390fcbb4edff5baf883cb65f672186fecf9b
Extacy <git@extacy.cc>
2021-09-21 17:11:14 +1200

    Add Servers constant

---
+++ src/constants/servers.ts
@@ -0,0 +1,76 @@
+import { IServer } from "..";
+
+export const Servers: IServer[] = [
+    {
+        "name": "USEast2",
+        "address": "52.87.248.5"
+    },
+    {
+        "name": "EUEast",
+        "address": "18.184.218.174"
+    },
+    {
+        "name": "EUSouthWest",
+        "address": "35.180.67.120"
+    },
+    {
+        "name": "EUNorth",
+        "address": "18.159.133.120"
+    },
+    {
+        "name": "USEast",
+        "address": "54.234.226.24"
+    },
+    {
+        "name": "USWest4",
+        "address": "54.235.235.140"
+    },
+    {
+        "name": "EUWest2",
+        "address": "52.16.86.215"
+    },
+    {
+        "name": "Asia",
+        "address": "3.0.147.127"
+    },
+    {
+        "name": "USSouth3",
+        "address": "52.207.206.31"
+    },
+    {
+        "name": "EUWest",
+        "address": "15.237.60.223"
+    },
+    {
+        "name": "USWest",
+        "address": "54.86.47.176"
+    },
+    {
+        "name": "USMidWest2",
+        "address": "3.140.254.133"
+    },
+    {
+        "name": "USMidWest",
+        "address": "3.133.12.23"
+    },
+    {
+        "name": "USSouth",
+        "address": "3.82.126.16"
+    },
+    {
+        "name": "USWest3",
+        "address": "18.144.30.153"
+    },
+    {
+        "name": "USSouthWest",
+        "address": "54.153.13.68"
+    },
+    {
+        "name": "USNorthWest",
+        "address": "34.238.176.119"
+    },
+    {
+        "name": "Australia",
+        "address": "13.236.87.250"
+    }
+];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./api/index";
+export * from "./constants/index";
 export * from "./realmlib/index";
 export * from "./tracker/index";
