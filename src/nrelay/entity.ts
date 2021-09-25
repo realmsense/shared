34512b4d275574b5e5849555f869ed565409890d
Extacy <git@extacy.cc>
2021-09-25 16:27:18 +1200

    Add objectType to Entity

--- src/nrelay/entity.ts
+++ src/nrelay/entity.ts
@@ -2,6 +2,7 @@ import { IWorldPosData } from "..";
 
 export interface IEntity {
     objectID: number;
+    objectType: number;
     pos: IWorldPosData;
     // condition: [ConditionEffect, ConditionEffect];
     // status: ObjectStatusData;
